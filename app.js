188eaf0bb3c61739aa6a1d3e7caf62b380b3b144
Zinanshin Kaoher <54826833+kaohzina@users.noreply.github.com>
2021-07-21 16:02:24 -0500

    new js and proper html loading

---
+++ app.js
@@ -0,0 +1,23 @@
+const generatePage = require('./src/page-template.js');
+
+const fs = require('fs');
+
+const profileDataArgs = process.argv.slice(2, process.argv.length);
+
+const [name, github] = profileDataArgs;
+
+// const printProfileData = profileDataArr => {
+//   for (let i = 0; i < profileDataArr.length; i += 1) {
+//     console.log(profileDataArr[i]);
+//   }
+//   console.log('===========');
+
+//   profileDataArr.forEach(profileItem => console.log(profileItem));
+// };
+// printProfileData(profileDataArgs);
+
+fs.writeFile('./index.html', generatePage(name, github), err => {
+  if (err) throw new Error(err);
+
+  console.log('Portfolio complete! Check out index.html to see the output!');
+});
