3b90f0765718b112ff7b3368251ee58cfc35827a
Zinanshin Kaoher <54826833+kaohzina@users.noreply.github.com>
2021-07-23 12:41:33 -0500

    prompt inquirer

--- app.js
+++ app.js
@@ -1,23 +1,21 @@
-const generatePage = require('./src/page-template.js');
+const inquirer = require('inquirer');
+// const generatePage = require('./src/page-template.js');
+// const fs = require('fs');
 
-const fs = require('fs');
+// const pageHTML = generatePage(name,github);
 
-const profileDataArgs = process.argv.slice(2, process.argv.length);
+// fs.writeFile('./index.html', generatePage(name, github), err => {
+//   if (err) throw err;
 
-const [name, github] = profileDataArgs;
+//   console.log('Portfolio complete! Check out index.html to see the output!');
+// });
 
-// const printProfileData = profileDataArr => {
-//   for (let i = 0; i < profileDataArr.length; i += 1) {
-//     console.log(profileDataArr[i]);
-//   }
-//   console.log('===========');
-
-//   profileDataArr.forEach(profileItem => console.log(profileItem));
-// };
-// printProfileData(profileDataArgs);
-
-fs.writeFile('./index.html', generatePage(name, github), err => {
-  if (err) throw new Error(err);
-
-  console.log('Portfolio complete! Check out index.html to see the output!');
-});
+inquirer
+  .prompt([
+    {
+      type: 'input',
+      name: 'name',
+      message: 'What is your name?'
+    }
+  ])
+.then(answers => console.log(answers));
