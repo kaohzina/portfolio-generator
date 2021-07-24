1cf3206d1ffaf7a6af16276887325b167bd0ba33
Zinanshin Kaoher <54826833+kaohzina@users.noreply.github.com>
2021-07-23 19:36:07 -0500

    if else statement to inquirer prompt

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const inquirer = require('inquirer');
+
 // const generatePage = require('./src/page-template.js');
 // const fs = require('fs');
 
@@ -10,12 +11,77 @@ const inquirer = require('inquirer');
 //   console.log('Portfolio complete! Check out index.html to see the output!');
 // });
 
-inquirer
-  .prompt([
+const promptUser = () => {
+  return inquirer.prompt([
     {
       type: 'input',
       name: 'name',
       message: 'What is your name?'
+    },
+    {
+      type: 'input',
+      name: 'github',
+      message: 'Enter your GitHub Username'
+    },
+    {
+      type: 'input',
+      name: 'about',
+      message: 'Provide some information about yourself:'
     }
-  ])
-.then(answers => console.log(answers));
+  ]);
+};
+
+
+const promptProject = portfolioData => {
+  if (!portfolioData.projects)
+  {portfolioData.projects = [];
+  }
+  console.log(`
+  =================
+  Add a New Project
+  =================
+  `);
+  return inquirer.prompt([
+    {
+      type: 'input',
+      name: 'description',
+      message: 'Provide a description of the project (Required)'
+    },
+    {
+      type: 'checkbox',
+      name: 'languages',
+      message: 'Whad did you build this project with? (Check all that apply)',
+      choices: ['Javascript','HTML','CSS','ES6','jQuery','Bootstrap','Node']
+    },
+    {
+      type: 'input',
+      name: 'link',
+      message: 'Enter the GitHub link to your project. (Required)'
+    },
+    {
+      type: 'confirm',
+      name: 'feature',
+      message: 'Would you like to feature this project?',
+      default: false
+    },
+    {
+      type: 'confirm',
+      name: 'confirmAddProject',
+      message: 'Would you like to enter another project?',
+      default: false
+    }
+  ]).then(projectData => {
+    portfolioData.projects.push(projectData);
+    if (projectData.confrimAddProject){
+      return promptProject(portfolioData);
+    } else {
+      return portfolioData;
+    }
+  });
+};
+
+promptUser()
+.then(promptProject)
+.then(portfolioData => {
+  console.log(portfolioData);
+});
