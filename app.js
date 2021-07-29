c141394dfb66e56590954f607ff0b46805259438
Zinanshin Kaoher <54826833+kaohzina@users.noreply.github.com>
2021-07-29 08:42:34 -0500

    updated-template

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const inquirer = require('inquirer');
 
-// const generatePage = require('./src/page-template.js');
-// const fs = require('fs');
+const fs = require('fs');
+const generatePage = require('./src/page-template');
 
 // const pageHTML = generatePage(name,github);
 
@@ -104,5 +104,11 @@ const promptProject = portfolioData => {
 promptUser()
 .then(promptProject)
 .then(portfolioData => {
-  console.log(portfolioData);
+  const pageHTML = generatePage(portfolioData);
+
+  // fs.writeFile('./index.html', pageHTML, err => {
+  // if (err) throw new Error(err);
+
+  // console.log('Page created! Check out index.html in this directory to see it!');
+  // });
 });
